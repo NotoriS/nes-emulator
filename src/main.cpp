19ddcf8db68f64c1d496d3efeff6212bcc095405
Evan Cestnick <evan@cestnick.com>
2025-01-06 23:03:47 -0400

    Moved the NesEmulator object's code into the main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,43 @@
 #include <iostream>
+#include <memory>
+#include <stdexcept>
 
-#include "nes-emulator.h"
+#include "cpu/cpu.h"
+#include "cpu/cpu-bus.h"
+#include "cartridge/cartridge.h"
 
 int main(int argc, char* argv[])
 {
+    // Fetch the ROM filepath from the program arguments
     if (argc < 2)
     {
         std::cerr << "No filename provided" << std::endl;
         return -1;
     }
+    const std::string romPath = argv[1];
 
-    NesEmulator emulator(argv[1]);
-    emulator.Run();
+    // Initalize the game cartridge
+    auto cartridge = std::make_shared<Cartridge>();
+    try
+    {
+        cartridge->LoadROM(romPath);
+    }
+    catch (const std::runtime_error& e)
+    {
+        std::cerr << "Fatal Error (" << e.what() << ")" << std::endl;
+        return -1;
+    }
+
+    // Initalize the CPU and main bus
+    auto cpuBus = std::make_shared<CpuBus>();
+    cpuBus->ConnectCartridge(cartridge);
+    auto cpu = std::make_unique<CPU>(cpuBus);
+
+    while (true)
+    {
+        // TODO: Modify to only clock at the specified clock rate.
+        cpu->Clock();
+    }
 
     return 0;
 }
