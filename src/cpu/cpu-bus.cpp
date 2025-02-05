218e2153f10e927593da67432dc17f95aa1bb786
Evan Cestnick <evan@cestnick.com>
2025-02-04 21:23:27 -0400

    Added comments to the CPU bus

--- src/cpu/cpu-bus.cpp
+++ src/cpu/cpu-bus.cpp
@@ -11,15 +11,15 @@ CpuBus::~CpuBus()
 
 uint8_t CpuBus::Read(uint16_t address)
 {
-    if (address >= 0x0000 && address <= 0x1FFF)
+    if (address >= 0x0000 && address <= 0x1FFF) // Read from RAM
     {
         return m_memory[address & 0x07FF];
     }
-    else if (address >= 0x2000 && address <= 0x3FFF)
+    else if (address >= 0x2000 && address <= 0x3FFF) // Read from PPU registers
     {
         return m_ppu->Read(address & 0x2007);
     }
-    else if (address >= 0x4020 && address <= 0xFFFF)
+    else if (address >= 0x4020 && address <= 0xFFFF) // Read from cartridge
     {
         return m_cartridge->CpuRead(address);
     }
@@ -30,17 +30,17 @@ uint8_t CpuBus::Read(uint16_t address)
 
 void CpuBus::Write(uint16_t address, uint8_t data)
 {
-    if (address >= 0x0000 && address <= 0x1FFF)
+    if (address >= 0x0000 && address <= 0x1FFF) // Write to RAM
     {
         m_memory[address & 0x07FF] = data;
         return;
     }
-    else if (address >= 0x2000 && address <= 0x3FFF)
+    else if (address >= 0x2000 && address <= 0x3FFF) // Write to PPU registers
     {
         m_ppu->Write(address & 0x2007, data);
         return;
     }
-    else if (address >= 0x4020 && address <= 0xFFFF)
+    else if (address >= 0x4020 && address <= 0xFFFF) // Write to cartridge
     {
         m_cartridge->CpuWrite(address, data);
         return;
