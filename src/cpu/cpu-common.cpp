66db1b7cf2377755961e7e84182ba6f4e95e0765
Evan Cestnick <evan@cestnick.com>
2025-02-20 22:10:30 -0400

    Added an explicit inline keyword to .cpp function implementations

--- src/cpu/cpu-common.cpp
+++ src/cpu/cpu-common.cpp
@@ -54,12 +54,12 @@ void CPU::Interrupt(InterruptType type)
     }
 }
 
-void CPU::Write(uint16_t address, uint8_t data)
+inline void CPU::Write(uint16_t address, uint8_t data)
 {
     if (m_bus != nullptr) m_bus->Write(address, data);
 }
 
-uint8_t CPU::Read(uint16_t address)
+inline uint8_t CPU::Read(uint16_t address)
 {
     if (m_bus != nullptr) return m_bus->Read(address);
     throw std::runtime_error("CPU tried to read from the bus before it was connected.");
