dcccf076477c554710dab244964228ca2d25082a
Evan Cestnick <evan@cestnick.com>
2025-04-06 16:06:18 -0300

    Implemented CPU write controls for mapper 1

--- src/cartridge/mappers/mapper-001.cpp
+++ src/cartridge/mappers/mapper-001.cpp
@@ -11,6 +11,39 @@ uint8_t Mapper001::CpuRead(uint16_t address)
 
 void Mapper001::CpuWrite(uint16_t address, uint8_t data)
 {
+    // TODO: Add support for PRG RAM
+    if (address < 0x8000)
+    {
+        Logger::GetInstance().Warn("A write to PRG RAMs address range was made, but it is not currently supported.");
+        return;
+    }
+
+    // Reset bit set
+    if (data & 0x80)
+    {
+        m_loadReg = 0b10000;
+        return;
+    }
+
+    // Load register is ready for copy
+    if (m_loadReg & 1)
+    {
+        uint8_t* targetReg;
+        if (address >= 0x8000 && address < 0xA000) targetReg = &m_controlReg;
+        else if (address >= 0xA000 && address < 0xC000) targetReg = &m_chrBankZeroReg;
+        else if (address >= 0xC000 && address < 0xE000) targetReg = &m_chrBankOneReg;
+        else if (address >= 0xE000) targetReg = &m_prgBankReg;
+        else throw std::runtime_error("unexpected path reached when copying MMC1's load register.");
+
+        *targetReg = (m_loadReg >> 1) & 0b01111;
+        *targetReg |= (data & 1) << 4;
+        m_loadReg = 0b10000;
+        return;
+    }
+
+    // shift and load the load register with bit 0
+    m_loadReg >>= 1;
+    m_loadReg |= (data & 1) << 4;
 }
 
 uint8_t Mapper001::PpuRead(uint16_t address)
--- src/cartridge/mappers/mapper-001.h
+++ src/cartridge/mappers/mapper-001.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <stdexcept>
 
 #include "../../debug/logger.h"
 #include "mapper.h"
